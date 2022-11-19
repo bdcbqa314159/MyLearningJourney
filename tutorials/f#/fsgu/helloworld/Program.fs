0f0555ae8ee61afe2bddc28605233978464d27b0
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-11-19 00:13:04 +0000

    Correcting the code in the helloworld project.

--- tutorials/f#/fsgu/helloworld/Program.fs
+++ tutorials/f#/fsgu/helloworld/Program.fs
@@ -1,2 +1,29 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+
+let sayHello person =
+    printfn "Hello %s from my f# program " person
+
+[<EntryPoint>]
+let main argv =
+    Array.iter sayHello argv
+    printfn "Nice to meet you"
+    0
+
+//[<EntryPoint>]
+// let main argv =
+// let person =
+//    if argv.Length > 0 then
+//        argv.[0]
+//    else
+//        "Anonymous Person"
+// printfn "Hello %s from my F# program!" person
+
+// for i in 0..argv.Length-1 do
+//     let person = argv.[i]
+//     printfn "Hello %s, nice to meet you " person
+
+// for person in argv do
+//     printfn "Hello %s, nice to meet you " person
+
+// printfn "Nice to meet you"
+// 0
