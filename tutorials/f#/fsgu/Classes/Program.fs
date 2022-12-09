cdbe8b53bd4601364519ab12ddf818b455f8d5ec
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-12-09 22:49:42 +0000

    Working with classes in f#.

---
+++ tutorials/f#/fsgu/Classes/Program.fs
@@ -0,0 +1,21 @@
+﻿open System
+open Classes
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from f#!"
+
+    let namePrompt = ConsolePrompt("Please enter your name",5)
+    namePrompt.BeepOnError <-false
+    namePrompt.ColorScheme<-ConsoleColor.Cyan, ConsoleColor.DarkGray
+    let name = namePrompt.GetValue()
+
+    printfn "Hello %s" name
+
+    let colorPrompt = ConsolePrompt("What is your favorite color (press Enter if you don't have one)",1)
+
+    let favoriteColor = colorPrompt.GetValue()
+    let person = Person(name, favoriteColor)
+    printfn "%s" (person.Description())
+
+    0
