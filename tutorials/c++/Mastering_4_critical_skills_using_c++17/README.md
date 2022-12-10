2d201b8c9d592cb5ee7553d485d43b6d34b46f39
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-12-10 15:42:34 +0000

    Cleaning the repository a bit.

---
+++ tutorials/c++/Mastering_4_critical_skills_using_c++17/README.md
@@ -0,0 +1,3 @@
+#Working with a c++ tutorial from Udemy
+
+tutorial can be found here : https://www.udemy.com/course/cpp-4skills/
---
+++ tutorials/c++/Mastering_4_critical_skills_using_c++17/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    std::cout << "Working with Udemy c++17 tutorial" << std::endl;
+
+    return 0;
+}
