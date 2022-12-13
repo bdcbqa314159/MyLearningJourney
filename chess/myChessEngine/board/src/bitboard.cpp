636b623033d406ceac634cce5052bcd6be5dd594
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2022-12-13 17:07:36 +0000

    Working with bitboards, pop and count in the chess engine project.

--- chess/myChessEngine/board/src/bitboard.cpp
+++ chess/myChessEngine/board/src/bitboard.cpp
@@ -23,21 +23,54 @@ void printBitBoard(U64 bitboard)
     }
 }
 
+// const std::array<int, 64> BitTable = {
+//     63, 30, 3, 32, 25, 41, 22, 33, 15, 50, 42, 13, 11, 53, 19, 34, 61, 29, 2,
+//     51, 21, 43, 45, 10, 18, 47, 1, 54, 9, 57, 0, 35, 62, 31, 40, 4, 49, 5, 52,
+//     26, 60, 23, 44, 46, 27, 56, 16, 7, 39, 48, 24, 59, 14, 12, 55, 38, 28,
+//     58, 20, 37, 17, 36, 8};
+
+int PopBit(U64 *bb)
+{
+    U64 b = *bb ^ (*bb - 1);
+    unsigned int fold = static_cast<unsigned>((b & 0xffffffff) ^ (b >> 32));
+    *bb &= (*bb - 1);
+    return BitTable[(fold * 0x783a9b23) >> 26];
+}
+
+int CountBits(U64 b)
+{
+    int r{};
+    for (r = 0; b; r++, b &= b - 1)
+        ;
+    return r;
+}
+
 void testingBitBoard()
 {
     std::cout << "From Bitboard" << std::endl;
     U64 playBitBoard = 0ULL;
-
-    std::cout << "Start: " << std::endl;
-    printBitBoard(playBitBoard);
+    // SQ64 in the videos means Sq120toSq64 here.
 
     playBitBoard |= (1ULL << Sq120ToSq64[D2]);
-    std::cout << "D2 added: " << std::endl;
-    printBitBoard(playBitBoard);
-    std::cout << std::endl;
+    playBitBoard |= (1ULL << Sq120ToSq64[D3]);
+    playBitBoard |= (1ULL << Sq120ToSq64[D4]);
 
-    playBitBoard |= (1ULL << Sq120ToSq64[G2]);
-    std::cout << "G2 added: " << std::endl;
-    printBitBoard(playBitBoard);
-    std::cout << std::endl;
+    // printBitBoard(playBitBoard);
+    // int count = CountBits(playBitBoard);
+
+    // std::cout << "Count: " << count << std::endl;
+
+    // int index = PopBit(&playBitBoard);
+    // std::cout << "Index: " << index << std::endl;
+    // printBitBoard(playBitBoard);
+    // count = CountBits(playBitBoard);
+
+    // std::cout << "Count: " << count << std::endl;
+    int sq64 = 0;
+    while (playBitBoard)
+    {
+        sq64 = PopBit(&playBitBoard);
+        std::cout << "popped: " << sq64 << std::endl;
+        printBitBoard(playBitBoard);
+    }
 }
