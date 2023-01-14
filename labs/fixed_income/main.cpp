137cda57c9725eaa8a340b4300a2c103b2621e45
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2023-01-14 00:01:46 +0000

    Working a bit on fixed income products.

--- labs/fixed_income/main.cpp
+++ labs/fixed_income/main.cpp
@@ -5,38 +5,89 @@
 
 using namespace std;
 
-class ZeroCouponBond
+class Bond
 {
+public:
+    Bond(double coupon, double price) : coupon(coupon), price(price)
+    {
+    }
+
+    double give_face_value(double investment)
+    {
+        face_value = 100 * investment / price;
+        return face_value;
+    }
+
+    double give_coupon_value()
+    {
+        return face_value * coupon * 0.01;
+    }
+
+    double give_total_atm()
+    {
+        return face_value * (1 + coupon * 0.01);
+    }
+
+private:
+    double coupon{}, price{}, face_value{};
 };
 
-class InterestRate
+class TBill
 {
+
+public:
+    TBill(double price, int n) : price(price), n(n)
+    {
+    }
+
+    double give_quote()
+    {
+        return 3.6 * (100 - price) / n;
+    }
+
+    double give_face_value(double investment)
+    {
+        face_value = 100 * investment / price;
+        return face_value;
+    }
+
+private:
+    double price{}, k{}, face_value{};
+    int n{};
 };
 
-class Price
+void working_first_example()
 {
-};
-
-void price_quotes()
-{
-    double price_of_the_bond = 102.1234;
-    double coupon = 5;
+    Bond myBond(5., 102.1234);
     double investment = 1000000;
+    cout << "FV = " << myBond.give_face_value(investment) << endl;
+    cout << "Coupon value = " << myBond.give_coupon_value() << endl;
+    cout << "At maturity we will receive = " << myBond.give_total_atm() << endl;
+}
 
-    double face_value = investment / (price_of_the_bond * 0.01);
-    double next_coupon_received = face_value * coupon * 0.01;
+void working_second_example()
+{
+    TBill myTBill(98.123456, 182);
 
-    double value_at_maturity = face_value * (1 + coupon * 0.01);
+    cout << "quote for this TBill = " << myTBill.give_quote() << endl;
+    double investment = 10000;
+    cout << "FV = " << myTBill.give_face_value(investment) << endl;
+}
 
-    cout << "If we invest " << investment << " we are able to buy the following face value (FV) : " << face_value << endl;
-    cout << "Which means we are going to receive a coupon of " << next_coupon_received << endl;
-    cout << "At maturity we are going to have " << value_at_maturity << endl;
+void working_3rd_example()
+{
+    cout << "With a semi annual coupon of 4%" << endl;
+    cout << "Between 15 Jan 2012 10 April 2012: " << endl;
+    cout << "act/act -> 0.02*86/182 = " << 0.02 * 86 / 182 << endl;
+    cout << "30/360 -> 0.02*85/180 = " << 0.02 * 85 / 180 << endl;
 }
 
 int main()
 {
     cout << "Fixed income" << endl;
-    price_quotes();
+    working_first_example();
+    working_second_example();
+    working_3rd_example();
 
     return 0;
 }
